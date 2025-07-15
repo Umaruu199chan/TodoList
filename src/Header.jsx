9d48b38a68aa4c189fe0c6dbdb0231f3f26ad09c
patrick <trickmeredor@gmail.com>
2025-07-15 12:24:37 +0800

    change the size of the text

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-    <h1 className="text-5xl text-center m-5 text-shadow-lg/20">Code <span className="text-red-500">PlayGround</span></h1>
+    <h1 className="text-3xl text-center m-5 text-shadow-lg/20">Code <span className="text-red-500">PlayGround</span></h1>
     )
 };
 
